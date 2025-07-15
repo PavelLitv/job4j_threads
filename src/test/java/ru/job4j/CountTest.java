c14d71dae46a9d57059d8dde3620508198cfdd52
Pavel Litvinenko <korvine1980@yandex.ru>
2025-07-15 22:15:15 +0500

    example

--- src/test/java/ru/job4j/CountTest.java
+++ src/test/java/ru/job4j/CountTest.java
@@ -1,12 +1,37 @@
 package ru.job4j;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CountTest {
+    @BeforeAll
+    public static void setUpBeforeClass() {
+        System.out.println("Before Class");
+    }
 
     @Test
     public void whenExecute2ThreadThen2() throws InterruptedException {
+        System.out.println(this);
+        var count = new Count();
+        var first = new Thread(count::increment);
+        var second = new Thread(count::increment);
+        /* Запускаем нити. */
+        first.start();
+        second.start();
+        /* Заставляем главную нить дождаться выполнения наших нитей. */
+        first.join();
+        second.join();
+        /* Проверяем результат. */
+        assertThat(count.get()).isEqualTo(2);
+    }
+
+    @Test
+    public void whenExecute2ThreadThen1() throws InterruptedException {
+        System.out.println(this);
         var count = new Count();
         var first = new Thread(count::increment);
         var second = new Thread(count::increment);
